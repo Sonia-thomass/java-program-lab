6478a532ec98be64edd57ea36122617483a4bc9d
Sonia-thomass <soniarosethomas7@gmail.com>
2025-06-10 20:06:34 +0530

    Add files via upload

---
+++ java/MatrixAdd.java
@@ -0,0 +1,50 @@
+Program 2 
+ 
+import java.util.Scanner; 
+public class MatrixAdd 
+ { 
+   public static void main(String args[]) 
+    { 
+      int row,col,i,j; 
+      Scanner s=new Scanner(System.in); 
+      System.out.println("Enter the number of rows: "); 
+      row=s.nextInt(); 
+      System.out.println("Enter the number of columns: "); 
+      col=s.nextInt(); 
+      int mat1[][]=new int[3][3]; 
+      int mat2[][]=new int[3][3]; 
+      int res[][]=new int[3][3]; 
+      System.out.println("Enter the elements of first matrix: "); 
+      for(i=0;i<row;i++) 
+       { 
+         for(j=0;j<col;j++) 
+          { 
+            mat1[i][j]=s.nextInt(); 
+          } 
+       } 
+  
+      System.out.println("Enter the elements of second matrix: "); 
+      for(i=0;i<row;i++) 
+       { 
+         for(j=0;j<col;j++) 
+          { 
+            mat2[i][j]=s.nextInt(); 
+          } 
+       } 
+      for(i=0;i<row;i++) 
+        for(j=0;j<col;j++) 
+          res[i][j]=mat1[i][j]+mat2[i][j]; 
+ 
+      System.out.println("Sum of two matrices "); 
+      for(i=0;i<row;i++) 
+       { 
+         for(j=0;j<col;j++) 
+          { 
+            System.out.print(res[i][j]+"  "); 
+          } 
+          System.out.println(); 
+       } 
+   } 
+ } 
+ 
+ 
